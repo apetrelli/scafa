a6acdf935a43750b3a7732138d92dc205acf3030
Antonio Petrelli <antonio.petrelli@eng.it>
2020-10-29 17:13:27 +0100

    Added sync DefaultProcessorFactory

---
+++ scafa-proto/src/main/java/com/github/apetrelli/scafa/proto/sync/processor/impl/DefaultProcessorFactory.java
@@ -0,0 +1,46 @@
+/**
+ * Scafa - A universal non-caching proxy for the road warrior
+ * Copyright (C) 2015  Antonio Petrelli
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.apetrelli.scafa.proto.sync.processor.impl;
+
+import com.github.apetrelli.scafa.proto.processor.Handler;
+import com.github.apetrelli.scafa.proto.processor.Input;
+import com.github.apetrelli.scafa.proto.processor.ProcessingContextFactory;
+import com.github.apetrelli.scafa.proto.processor.Processor;
+import com.github.apetrelli.scafa.proto.processor.ProcessorFactory;
+import com.github.apetrelli.scafa.proto.sync.SyncSocket;
+import com.github.apetrelli.scafa.proto.sync.processor.InputProcessorFactory;
+
+public class DefaultProcessorFactory<P extends Input, H extends Handler>
+		implements ProcessorFactory<H, SyncSocket> {
+
+	private InputProcessorFactory<H, P> inputProcessorFactory;
+
+	private ProcessingContextFactory<P> processingContextFactory;
+
+	public DefaultProcessorFactory(InputProcessorFactory<H, P> inputProcessorFactory,
+			ProcessingContextFactory<P> processingContextFactory) {
+		this.inputProcessorFactory = inputProcessorFactory;
+		this.processingContextFactory = processingContextFactory;
+	}
+
+	@Override
+	public Processor<H> create(SyncSocket client) {
+		return new DefaultProcessor<>(client, inputProcessorFactory, processingContextFactory);
+	}
+
+}
