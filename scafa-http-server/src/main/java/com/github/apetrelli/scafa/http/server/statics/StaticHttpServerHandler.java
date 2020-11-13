01b14247e86e8b981257ca71bda4bb31d78a537a
Antonio Petrelli <antonio.petrelli@eng.it>
2020-11-13 17:27:15 +0100

    Added redirect.

--- scafa-http-server/src/main/java/com/github/apetrelli/scafa/http/server/statics/StaticHttpServerHandler.java
+++ scafa-http-server/src/main/java/com/github/apetrelli/scafa/http/server/statics/StaticHttpServerHandler.java
@@ -50,36 +50,42 @@ public class StaticHttpServerHandler extends HttpServerHandlerSupport {
 		if ("GET".equals(request.getMethod())) {
 			if (request.getResource().startsWith(basePath)) {
 				String localResource = request.getParsedResource().getResource().substring(basePath.length());
-				while (localResource.startsWith("/")) {
-					localResource = localResource.substring(1);
-				}
-				if (localResource.isEmpty()) {
-					localResource = indexResource;
-				}
-				if (!localResource.contains("..")) {
-					Path path = FileSystems.getDefault().getPath(baseFilesystemPath, localResource);
-					if (Files.exists(path)) {
-						HttpResponse response = new HttpResponse("HTTP/1.1", 200, "Found");
-						response.setHeader("Server", "Scafa");
-						response.setHeader("Date", getCurrentDateString());
-						String connection = request.getHeader("Connection");
-						if (!"close".equals(connection)) {
-							connection = "keep-alive";
-						}
-						response.setHeader("Connection", connection);
-						int dotPosition = localResource.lastIndexOf('.');
-						if (dotPosition >= 0) {
-							String contentType = mimeTypeConfig.get(localResource.substring(dotPosition + 1).toLowerCase());
-							if (contentType != null) {
-								response.setHeader("Content-Type", contentType);
-							}
-						}
-						return server.response(channel, response, path);
-					} else {
-						return sendSimpleMessage(404, "The resource " + localResource + " does not exist");
-					}
+				if (localResource.isEmpty() && !basePath.equals("/")) {
+					HttpResponse response = createSimpleResponse(302, "Found");
+					response.setHeader("Location", basePath + "/");
+					return server.response(channel, response);
 				} else {
-					return sendSimpleMessage(400, "No path traversal");
+					while (localResource.startsWith("/")) {
+						localResource = localResource.substring(1);
+					}
+					if (localResource.isEmpty()) {
+						localResource = indexResource;
+					}
+					if (!localResource.contains("..")) {
+						Path path = FileSystems.getDefault().getPath(baseFilesystemPath, localResource);
+						if (Files.exists(path)) {
+							HttpResponse response = new HttpResponse("HTTP/1.1", 200, "Found");
+							response.setHeader("Server", "Scafa");
+							response.setHeader("Date", getCurrentDateString());
+							String connection = request.getHeader("Connection");
+							if (!"close".equals(connection)) {
+								connection = "keep-alive";
+							}
+							response.setHeader("Connection", connection);
+							int dotPosition = localResource.lastIndexOf('.');
+							if (dotPosition >= 0) {
+								String contentType = mimeTypeConfig.get(localResource.substring(dotPosition + 1).toLowerCase());
+								if (contentType != null) {
+									response.setHeader("Content-Type", contentType);
+								}
+							}
+							return server.response(channel, response, path);
+						} else {
+							return sendSimpleMessage(404, "The resource " + localResource + " does not exist");
+						}
+					} else {
+						return sendSimpleMessage(400, "No path traversal");
+					}
 				}
 			} else {
 				return sendSimpleMessage(404, "Resource " + request.getResource() +  " not found");
@@ -90,9 +96,14 @@ public class StaticHttpServerHandler extends HttpServerHandlerSupport {
 	}
 
 	private CompletableFuture<Void> sendSimpleMessage(int httpCode, String message) {
+		HttpResponse response = createSimpleResponse(httpCode, message);
+		return server.response(channel, response);
+	}
+
+	private HttpResponse createSimpleResponse(int httpCode, String message) {
 		HttpResponse response = new HttpResponse("HTTP/1.1", httpCode, message);
 		response.setHeader("Server", "Scafa");
 		response.setHeader("Content-Length", "0");
-		return server.response(channel, response);
+		return response;
 	}
 }
