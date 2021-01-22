c2397b067c0caf3f385fdbc6a23eee8349ccc45e
Antonio Petrelli <antonio.petrelli@eng.it>
2021-01-22 12:19:13 +0100

    Removal of experimental sync code.

--- scafa-proto/src/main/java/module-info.java
+++ scafa-proto/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.github.apetrelli.scafa.proto {
 	requires transitive com.github.apetrelli.scafa.tls;
 	requires java.logging;
 	exports com.github.apetrelli.scafa.proto.aio;
-	exports com.github.apetrelli.scafa.proto.aio.sync;
 	exports com.github.apetrelli.scafa.proto.aio.impl;
 	exports com.github.apetrelli.scafa.proto.client;
 	exports com.github.apetrelli.scafa.proto.client.impl;
