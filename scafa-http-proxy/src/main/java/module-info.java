2d61204d66b40b1508c77447ce3cf223388cda0f
Antonio Petrelli <antonio.petrelli@gmail.com>
2022-06-07 15:00:41 +0200

    Moving code to scafa-sync-http-gateway.

--- scafa-http-proxy/src/main/java/module-info.java
+++ scafa-http-proxy/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.github.apetrelli.scafa.http.proxy {
-	requires transitive com.github.apetrelli.scafa.http.gateway;
 	requires transitive com.github.apetrelli.scafa.async.http.gateway;
+	requires transitive com.github.apetrelli.scafa.sync.http.gateway;
 	requires java.logging;
     requires jcifs;
 	exports com.github.apetrelli.scafa.http.proxy;
